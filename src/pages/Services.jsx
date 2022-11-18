68cafa848d01119330aacc1dfe0292674de124a2
SaracensFan <matthewclifford@hotmail.co.uk>
2022-11-18 12:17:07 +0000

    update

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,6 +1,31 @@
+import services from "../services"
+
 export default function Services() {
     return (
-      <h1>Services</h1>
+      <div>
+        <h1>Services</h1>
+
+        <div className="row">
+        {services.map((service) => {
+          return (
+
+              <div className="col-md-6 mb-3">
+                <div className="card">
+                  <div className="card-header">
+                    <h4>{service.title}</h4>
+                  </div>
+                  <div className="card-body">
+                    Click
+                  </div>
+                  <div className="card-footer">
+                    Footer
+                  </div>
+                </div>
+              </div>
+          )
+        })}
+            </div>
+      </div>
     )
   };
   
