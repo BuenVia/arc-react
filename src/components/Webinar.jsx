3885f4130956b242ad970d9a792eab8c233cf75f
SaracensFan <matthewclifford@hotmail.co.uk>
2022-11-26 18:31:18 +0000

    update

--- src/components/Webinar.jsx
+++ src/components/Webinar.jsx
@@ -1,10 +1,28 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import LoadingSpinner from "./LoadingSpinner"
+
 export default function Webinar() {
 
-    const webinar = {
-        title: 'Webinar Title',
-        date: new Date(),
-        time: '12:30'
-    }
+  const [loading, setLoading] = useState(false)
+  const [webinar, setWebinar] = useState({
+    title: 'Webinar Title',
+    date: '',
+    time: '',
+    link: ''
+  })
+
+  const loadWebinar = () => {
+    axios
+    .get('https://arcsupportservices-api.onrender.com/api/webinar')
+    .then(res => setWebinar(res.data[0]))
+    .catch(err => console.error(err))
+    setLoading(true)
+  }
+
+  useEffect(() => {
+   loadWebinar()
+  }, [])
 
     return (
         <div className="card">
@@ -12,12 +30,17 @@ export default function Webinar() {
           <h4>Webinar</h4>
         </div>
           <div className="card-body">
+          {loading ? 
+          <div>
             <p><span className="card-bold">Title:</span> {webinar.title}</p>
-            <p><span className="card-bold">Date:</span> {webinar.date.toDateString()}</p>
+            <p><span className="card-bold">Date:</span> {new Date(webinar.date).toDateString()}</p>
             <p><span className="card-bold">Time:</span> {webinar.time}</p>
           </div>
+          : 
+          <LoadingSpinner /> }
+          </div>
           <div className="card-footer">
-            {webinar && <a href="https://formsite.com" target="__blank" className="btn btn-link">Sign-Up</a>}
+            {webinar && <a href={webinar.link} target="__blank" className="btn btn-link">Sign-Up</a>}
           </div>
         </div>
     )
